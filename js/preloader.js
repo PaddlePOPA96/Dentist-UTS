4713ee170d578d4456c593b4f30cea0e03a8d236
PaddlePOPA96 <93701809+PaddlePOPA96@users.noreply.github.com>
2022-11-27 15:16:47 +0700

    Add files via upload

---
+++ js/preloader.js
@@ -0,0 +1,19 @@
+  // script for loading 
+  var loader = document.getElementById("preloader");
+  window.addEventListener("load", function(){
+    loader.style.display = "none"
+  })
+
+
+let menu = document.querySelector("#menu-btn");
+let navbar = document.querySelector(".header .navbar");
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
